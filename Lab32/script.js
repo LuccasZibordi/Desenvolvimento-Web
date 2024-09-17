ca2005553b32090aab20ab44158b51a1e735d069
Luccas Zibordi <luccaszibordi@gmail.com>
2024-09-16 21:37:43 -0300

    Adição de um botão para trocar os temas de claro e escuro da pagina ( precisa terminar )

--- Lab32/script.js
+++ Lab32/script.js
@@ -123,3 +123,9 @@ nextButton.addEventListener("click", ()=>{
 })
 
 startQuiz();
+
+const themeButton= document.querySelector('#theme')
+
+themeButton.addEventListener('click',function(){
+    $html.classList.toggle('light-mode')
+})
